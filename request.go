a8510a985df7d9123f307aca4332a24f2caba0b1
Victor Yunevich <v.t.g.m.b.x@gmail.com>
2014-12-10 20:08:22 +0300

    cleaning Request initialization

--- request.go
+++ request.go
@@ -12,61 +12,24 @@ import (
 
 // Request gathers all information about request
 type Request struct {
-	ID          int64  // storing record ID from URL
-	Root        string // default JSON root key
-	action      string
-	req         *http.Request
-	params      map[string]interface{}
-	renderJSON  func(int, JSONData)
-	render      func(int, string)
-	renderError func(int, string)
-	loadFile    func(string, string) (string, error)
+	ID     int64  // storing record ID from URL
+	Root   string // default JSON root key
+	action string
+	params map[string]interface{}
+
+	req *http.Request
+	w   http.ResponseWriter
 }
 
 // Init initializing controller
 func (r *Request) Init(w http.ResponseWriter, req *http.Request, root, prefix string) {
 	r.req = req
+	r.w = w
 	r.Root = root
 
 	urlParts := urlParts(strings.TrimPrefix(req.URL.Path, prefix))
 	r.ID, _ = strconv.ParseInt(urlParts[0], 10, 64)
 	r.action = r.makeAction(req.Method, urlParts)
-
-	if strings.Contains(req.Header.Get("Accept-Encoding"), "gzip") {
-		r.renderJSON = func(code int, s JSONData) {
-			RenderJSONgzip(w, code, s)
-		}
-	} else {
-		r.renderJSON = func(code int, s JSONData) {
-			RenderJSON(w, code, s)
-		}
-	}
-
-	r.render = func(code int, s string) {
-		w.WriteHeader(code)
-		w.Write([]byte(s))
-	}
-
-	r.renderError = func(code int, s string) {
-		http.Error(w, s, code)
-	}
-
-	r.loadFile = func(field, dir string) (string, error) {
-		r.req.ParseMultipartForm(32 << 20)
-		file, handler, err := r.req.FormFile(field)
-		if err != nil {
-			return "", err
-		}
-		defer file.Close()
-		fmt.Fprintf(w, "%v", handler.Header)
-		f, err := os.OpenFile(dir+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-		if err != nil {
-			return "", err
-		}
-		defer f.Close()
-		io.Copy(f, file)
-		return handler.Filename, nil
-	}
 
 	r.params = make(map[string]interface{})
 }
@@ -142,25 +105,43 @@ func (r *Request) CurrentAction() string {
 
 // RenderJSON rendering JSON to client
 func (r *Request) RenderJSON(code int, s JSONData) {
-	r.renderJSON(code, s)
+	if strings.Contains(r.req.Header.Get("Accept-Encoding"), "gzip") {
+		RenderJSONgzip(r.w, code, s)
+	} else {
+		RenderJSON(r.w, code, s)
+	}
 }
 
 // RenderJSONError rendering error to client in JSON format
 func (r *Request) RenderJSONError(code int, s string) {
-	r.renderJSON(code, JSONData{"errors": JSONData{"message": []string{s}}})
+	r.RenderJSON(code, JSONData{"errors": JSONData{"message": []string{s}}})
 }
 
 // Render rendering string to client
 func (r *Request) RenderString(code int, s string) {
-	r.render(code, s)
+	r.w.WriteHeader(code)
+	r.w.Write([]byte(s))
 }
 
 // RenderError rendering error to client
 func (r *Request) RenderError(code int, s string) {
-	r.renderError(code, s)
+	http.Error(r.w, s, code)
 }
 
 // LoadFile handling file uploads
 func (r *Request) LoadFile(field, dir string) (string, error) {
-	return r.loadFile(field, dir)
+	r.req.ParseMultipartForm(32 << 20)
+	file, handler, err := r.req.FormFile(field)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	fmt.Fprintf(r.w, "%v", handler.Header)
+	f, err := os.OpenFile(dir+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	io.Copy(f, file)
+	return handler.Filename, nil
 }
