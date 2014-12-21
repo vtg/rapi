e56877befea9cdc874e92293f2b21e32bf13c19c
Victor Yunevich <v.t.g.m.b.x@gmail.com>
2014-12-21 23:15:59 +0300

    Extracting ModelBase structure

--- model.go
+++ model.go
@@ -10,29 +10,24 @@ import (
 // ModelErrors errors type
 type ModelErrors map[string][]string
 
-// Model structure for base model
+// ModelBase structure for base model
 //	type User struct {
-// 	    rapi.Model
+// 	    rapi.ModelBase
 // 	    Name string
 // 	}
-type Model struct {
-	Id        int64       `json:"id"`
+type ModelBase struct {
 	CreatedAt time.Time   `json:"createdAt"`
 	UpdatedAt time.Time   `json:"updatedAt"`
 	Errors    ModelErrors `sql:"-" json:"-"`
 }
 
-// ID returns ID of record
-func (m *Model) ID() int64 {
-	return m.Id
-}
-
-func (m *Model) ResetErrors() {
+// ResetErrors clean all model errors
+func (m *ModelBase) ResetErrors() {
 	m.Errors = make(ModelErrors)
 }
 
 // AddError adding error to record
-func (m *Model) AddError(f string, t string) {
+func (m *ModelBase) AddError(f string, t string) {
 	if m.IsValid() {
 		m.Errors = make(ModelErrors)
 	}
@@ -40,7 +35,7 @@ func (m *Model) AddError(f string, t string) {
 }
 
 // IsValid returns true if no errors on record
-func (m *Model) IsValid() bool {
+func (m *ModelBase) IsValid() bool {
 	return len(m.Errors) == 0
 }
 
@@ -49,23 +44,23 @@ func (m *Model) IsValid() bool {
 //  	u.ValidatePresence("Name", u.Name)
 //  	return u.IsValid()
 //  }
-func (m *Model) Valid() bool {
+func (m *ModelBase) Valid() bool {
 	return m.IsValid()
 }
 
 // GetErrors returns record errors
-func (m *Model) GetErrors() ModelErrors {
+func (m *ModelBase) GetErrors() ModelErrors {
 	return m.Errors
 }
 
 // SetErrors set record errors
-func (m *Model) SetErrors(e ModelErrors) {
+func (m *ModelBase) SetErrors(e ModelErrors) {
 	m.Errors = e
 }
 
 // ValidatePresence validates string for presence
 // 	m.ValidatePresence("Name", m.Name)
-func (m *Model) ValidatePresence(f, v string) {
+func (m *ModelBase) ValidatePresence(f, v string) {
 	if utf8.RuneCountInString(v) == 0 {
 		m.AddError(f, "can't be blank")
 	}
@@ -73,7 +68,7 @@ func (m *Model) ValidatePresence(f, v string) {
 
 // ValidateLength validates string min, max length. -1 for any
 // 	m.ValidateLength("password", m.Password, 6, 18) // min 6, max 18
-func (m *Model) ValidateLength(f, v string, min, max int) {
+func (m *ModelBase) ValidateLength(f, v string, min, max int) {
 	if min > 0 {
 		if utf8.RuneCountInString(v) < min {
 			m.AddError(f, fmt.Sprint("minimum length is", min))
@@ -88,7 +83,7 @@ func (m *Model) ValidateLength(f, v string, min, max int) {
 
 // ValidateInt validates int min, max. -1 for any
 // 	m.ValidateInt("number", 10, -1, 11)  // max 18
-func (m *Model) ValidateInt(f string, v, min, max int) {
+func (m *ModelBase) ValidateInt(f string, v, min, max int) {
 	if min > 0 {
 		if v < min {
 			m.AddError(f, fmt.Sprint("minimum length is", min))
@@ -103,7 +98,7 @@ func (m *Model) ValidateInt(f string, v, min, max int) {
 
 // ValidateInt64 validates int64 min, max. -1 for any
 // 	m.ValidateInt64("number", 10, 6, -1) // min 6
-func (m *Model) ValidateInt64(f string, v, min, max int64) {
+func (m *ModelBase) ValidateInt64(f string, v, min, max int64) {
 	if min > 0 {
 		if v < min {
 			m.AddError(f, fmt.Sprint("minimum length is", min))
@@ -118,7 +113,7 @@ func (m *Model) ValidateInt64(f string, v, min, max int64) {
 
 // ValidateFloat32 validates float32 min, max. -1 for any
 // 	m.ValidateFloat32("number", 10.2, -1, 11)
-func (m *Model) ValidateFloat32(f string, v, min, max float32) {
+func (m *ModelBase) ValidateFloat32(f string, v, min, max float32) {
 	if min > 0 {
 		if v < min {
 			m.AddError(f, fmt.Sprint("minimum length is", min))
@@ -133,7 +128,7 @@ func (m *Model) ValidateFloat32(f string, v, min, max float32) {
 
 // ValidateFloat64 validates float64 min, max. -1 for any
 // 	m.ValidateFloat64("number", 10.2, -1, 11)
-func (m *Model) ValidateFloat64(f string, v, min, max float64) {
+func (m *ModelBase) ValidateFloat64(f string, v, min, max float64) {
 	if min > 0 {
 		if v < min {
 			m.AddError(f, fmt.Sprint("minimum length is", min))
@@ -148,10 +143,25 @@ func (m *Model) ValidateFloat64(f string, v, min, max float64) {
 
 // ValidateFormat validates string format with regex string
 // 	m.ValidateFormat("ip address", u.IP, `\A(\d{1,3}\.){3}\d{1,3}\z`)
-func (m *Model) ValidateFormat(f, v, reg string) {
+func (m *ModelBase) ValidateFormat(f, v, reg string) {
 	if r, _ := regexp.MatchString(reg, v); !r {
 		m.AddError(f, "invalid format")
 	}
+}
+
+// Model structure for base model with ID included
+//	type User struct {
+// 	    rapi.Model
+// 	    Name string
+// 	}
+type Model struct {
+	Id int64 `json:"id"`
+	ModelBase
+}
+
+// ID returns ID of record
+func (m *Model) ID() int64 {
+	return m.Id
 }
 
 // BaseModel interface
